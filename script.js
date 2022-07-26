3cbe0b612a3549f6371cfd875a1808b9748c54ec
Vivi - pessoal <martini.viviane@gmail.com>
2022-07-26 12:30:57 -0300

    add improvements

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-ScrollReveal().reveal('.headline', { delay: 500 });
-ScrollReveal().reveal('.tagline', { delay: 800 });
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '50px',
+    duration: 2000,
+    reset: true
+    });
+    
+ScrollReveal().reveal('.title', { delay: 200 });
+ScrollReveal().reveal('.subtitle', { delay: 400 });
